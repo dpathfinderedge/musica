9385a3466fed13494f56ac04d3437d55580dbb05
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2022-12-28 07:22:14 +0100

    Removed playlist bg on page load

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { Hero, TopCharts, NewRelease, Popular, Recent } from './';
+
+const Home = () => {
+  const { setPlaylistBG, recentlyPlayed } = useStateContext();
+
+  // Remove playlist bg when homepage renders
+  useEffect(() => {
+    setPlaylistBG(false);
+  });
+
+  return (
+    <div className="w-full h-screen overflow-y-auto overflow-x-hidden md:mr-6 mr-2">
+      <section className="flex md:flex-row flex-col gap-6 w-full">
+        <Hero />
+        <TopCharts />
+      </section>
+      <section>
+        <NewRelease />
+      </section>
+      <section>
+        <Popular />
+      </section>
+      <section>
+        {recentlyPlayed?.length > 0  && <Recent />}
+      </section>
+    </div>
+  )
+}
+
+export default Home;
