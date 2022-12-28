5e73f25483b3e9b780273cccebf69456dd0ae4df
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2022-12-28 07:16:50 +0100

    Fixed player bug

---
+++ src/pages/home/Recent.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import MusicList from './MusicList';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const Recent = () => {
+  const { recentlyPlayed, showPlayer } = useStateContext();
+  const sortedTracks = recentlyPlayed.sort((a, b) => b.timestamp - a.timestamp);
+
+  return (
+    <div className={`-mt-24 ${showPlayer ? 'mb-32' : 'mt-7 mb-10'}`}>
+      <h2 className="text-2xl font-bold mb-3.5">Recently played</h2>
+      <div className="flex flex-nowrap gap-7 overflow-x-scroll w-full">
+        {sortedTracks.length && sortedTracks.map((track) => ( 
+          <MusicList 
+          key={track?.data?.id}
+          index={track?.data?.index}
+          img={track?.data?.image}
+          title={track?.data?.title}
+          artist={track?.data?.artist}
+          url={track?.data?.url}
+         />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Recent;
