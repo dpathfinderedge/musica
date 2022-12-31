318a9e401d2159fb036be43a51af707dc33004f3
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-01 00:31:05 +0100

    Fixed player bug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Player, SideNav } from "./components";
+import Header from "./components/Header";
+import { useStateContext } from "./contexts/ContextProvider";
+import { Collection, Home, Playlist } from "./pages";
 
 function App() {
+  const { playlistBG } = useStateContext();
+
+  const styles={
+    backgroundImage: `linear-gradient(180deg, rgba(29, 33, 35, 0.8) 0%, #1D2123 61.48%), url(${playlistBG})`
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="h-screen overflow-y-auto overflow-x-hidden relative bg-main-bg px-6 bg-cover bg-no-repeat bg-center" style={styles}>
+          <Player />
+          <Header />
+          <main className="flex w-full h-full text-white mt-20"> {/** gap-6 */}
+            <SideNav />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/home" element={<Home />} />
+              <Route exact path="/collection" element={<Collection />} />
+              <Route exact path="/playlist/:playlistId" element={<Playlist />} />
+              <Route exact path="/radio" element={<>Coming Soon...</>} />
+              <Route exact path="/videos" element={<>Coming Soon...</>} />
+              <Route exact path="/profile" element={<>Coming Soon...</>} />
+              <Route exact path="/logout" element={<>Coming Soon...</>} />
+              <Route path="*" element={<>Oops, that page cannot not be found 😞</>} />
+            </Routes>
+          </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
