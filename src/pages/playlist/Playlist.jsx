1b234e9de03586d9f65e4526a8d8fd60100ad4d5
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-01 00:19:26 +0100

    Improved responsiveness and fixed a bug

--- src/pages/playlist/Playlist.jsx
+++ src/pages/playlist/Playlist.jsx
@@ -10,23 +10,31 @@ import { chartItems } from '../home/chartItems';
 import PlaylistCard from './PlaylistCard';
 
 const Playlist = () => {
-  const { setPlaylistBG, playTrack, RNB, hipHop, tomorrowTune, addToCollections, removeFromCollections, showPlayer } = useStateContext();
+  const { setPlaylistBG, playTrack, RNB, hipHop, tomorrowTune, myCollections, addToCollections, removeFromCollections, showPlayer } = useStateContext();
   const [currentPlaylist, setCurrentPlaylist] = useState({});
-  const [playlistAdded, setPlaylistAdded] = useState(false);
+  const [playlistAddedToCollection, setPlaylistAddedToCollection] = useState(false);
   const {playlistId} = useParams();
   const [trackState, setTrackState] = useState([]);
 
   useEffect(() => {
     // Get the playlist that matches the id
-    // eslint-disable-next-line eqeqeq
     const current = chartItems.filter(item => item.id == playlistId );
     
     setCurrentPlaylist(current[0]);
+
   },[playlistId]);
 
   useEffect(() => {
     setPlaylistBG(currentPlaylist.images);
   });
+
+  useEffect(() => {
+    if (currentPlaylist && myCollections) {
+      const check = myCollections.filter(data => data.id === currentPlaylist.id );
+
+      check.length === 1 && setPlaylistAddedToCollection(true);
+    }
+  }, [currentPlaylist]);
   
   const handleClick = (e) => {
     playTrack(e, trackState);
@@ -34,7 +42,6 @@ const Playlist = () => {
 
   const handleOptions = (e, text) => {
     e.preventDefault();
-    // console.log(e)
     
     if (text === 'Play all') {
       playTrack(e, trackState);
@@ -42,21 +49,19 @@ const Playlist = () => {
      
     if (text === 'Add to collection') {
       addToCollections(currentPlaylist);
-      setPlaylistAdded(true);
+      setPlaylistAddedToCollection(true);
     }
 
     if (text === 'Remove from collection') {
-      removeFromCollections(playlistId);
-      setPlaylistAdded(false);
+      removeFromCollections(currentPlaylist.id);
+      setPlaylistAddedToCollection(false);
     }
   };
 
   
   useEffect(() => {
-    // eslint-disable-next-line eqeqeq
       if (playlistId == 0) {
         setTrackState(RNB);
-        // eslint-disable-next-line eqeqeq
       } else if (playlistId == 1) {
         setTrackState(hipHop);
       } else {
@@ -69,24 +74,22 @@ const Playlist = () => {
       <Animation>
         <section className="w-full h-screen overflow-auto">
           <div className="flex md:flex-row flex-col md:justify-start justify-center md:items-end items-start gap-6 w-full">
-            <div className="w-72 h-full">
+            <div className="sm:w-72 w-full h-full">
               <img className="object-cover rounded-[35px] w-full h-full" src={currentPlaylist?.images} alt="" />
             </div>
             <article className="w-full">
               <h4 className="text-4xl font-bold mb-2 text-[#A4C7C6]">{currentPlaylist?.title}</h4>
               <p className="text-sm text-[#EFEEE0] mb-2.5 max-w-[33rem]">{currentPlaylist?.desc}</p>
               <p className="text-sm text-[#EFEEE0]">10 songs - 1hr+</p>
-              <div className="flex gap-3 mt-6 whitespace-nowrap w-full">
+              <div className="flex gap-3 mt-6 w-full">
                 {[
                   [playIcon, 'Play all'],
-                  [collectionIcon, playlistAdded ? 'Remove from collection' : 'Add to collection'],
+                  [collectionIcon, playlistAddedToCollection ? 'Remove from collection' : 'Add to collection'],
                   [heartIcon]
                 ].map(([src, text]) => (
-                  <>
-                    <button key={src} data-id={0} onClick={(e) => handleOptions(e,text)} className="play-button outline-0 border-0">
-                      <img className={text ? 'mr-2' : '' } src={src} alt="icon" /> {text}
-                    </button>
-                  </>
+                  <button key={src} data-id={0} onClick={(e) => handleOptions(e,text)} className="play-button outline-0 border-0">
+                    <img className={text ? 'mr-1' : '' } src={src} alt="icon" /> {text}
+                  </button>
                 ))}
               </div>
             </article>  
@@ -94,7 +97,7 @@ const Playlist = () => {
           <div className={`mt-5 md:mr-8 mr-0 ${showPlayer ? 'sm:mb-32 mb-28' : 'mb-5'}`}> 
             {trackState.map((item, index) => (
               <PlaylistCard
-                key={item?.id}
+                key={index}
                 index={index}
                 img={item?.images?.coverart}
                 title={item?.title}
