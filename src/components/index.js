d717d987f6baf98017114d31882a870d9d54b883
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-01 00:26:35 +0100

    Added navigations

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './Header';
+export { default as MobileNav } from './MobileNav';
+export { default as SideNav } from './SideNav';
+export { default as Player } from './Player';
+export { default as Animation } from './Animation';
