720172909bc1ecb03b2f6dfe13f92548dbad8f3f
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-05 15:13:18 +0100

    refactor: Refactored Tailwind's flexbox class - Changed gap to space

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -8,13 +8,13 @@ const MobileNav = ({ handleCloseSideMenu}) => {
       className={`md:hidden fixed inset-0 -left-full w-full slide-down transition-all pt-20 pl-10 bg-dark-alt z-50`}
     > 
       <button onClick={handleCloseSideMenu} className="absolute text-white top-4 right-4 text-2xl" >X</button>
-      <div className='flex flex-col gap-6'>
+      <div className='flex flex-col space-y-6'>
         {mobileNav.map((item) => (
           <NavLink 
             key={item.name}
             to={`/${item.name}`} 
             onClick={handleCloseSideMenu}
-            className="flex gap-3"
+            className="flex space-x-3"
           >
             <img src={item.icon} alt={item.icon} />
             <span className="capitalize hover:text-gray-300">{item.name}</span>
