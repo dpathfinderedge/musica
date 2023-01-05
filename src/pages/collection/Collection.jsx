85e4373a7bfdb6cd9329a199db991f71f7fff9be
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-05 15:14:29 +0100

    refactor: Refactored Tailwind's flexbox class - Changed gap to space

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -9,12 +9,12 @@ const Collection = () => {
   return (
     <Animation>
       <section className="text-sm w-full h-screen overflow-auto">
-        <div className="flex items-center gap-2.5">
+        <div className="flex items-center space-x-2.5">
           <button className="text-primary-dark bg-primary-yellow rounded-full py-2.5 px-4">My Collections</button>
           <button className="text-[#EFEEE0] border border-[#EFEEE0] rounded-full py-2.5 px-4 opacity-25">My Favorites</button>
         </div>
         {(typeof(myCollections) === 'undefined' || myCollections.length === 0 ) && <div className="mt-3 text-lg text-[#EFEEE0] opacity-2">Oops, your collection seems to be empty</div>}
-        <div className={`flex sm:flex-row flex-col gap-5 mt-6 ${showPlayer ? 'sm:mb-0 mb-28' : 'mb-5'}`}>
+        <div className={`flex sm:flex-row flex-col sm:space-x-5 space-x-0 md:space-y-0 space-y-5 mt-6 ${showPlayer ? 'sm:mb-0 mb-28' : 'mb-5'}`}>
           {typeof(myCollections) !== 'undefined' && myCollections.map((collection, index) => (
             <CollectionCard
               key={index}
