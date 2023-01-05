05a3ed1a3a1bccdcfd7ac217d4b0cc1c57127d44
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-05 15:18:35 +0100

    refactor: Refactored Tailwind's flexbox class - Changed gap to space

--- src/pages/playlist/PlaylistCard.jsx
+++ src/pages/playlist/PlaylistCard.jsx
@@ -9,16 +9,16 @@ const PlaylistCard = ({ index, img, title, category, duration, artist, handleCli
   return (
     <button onClick={handleClick} data-id={index} className="flex items-center backdrop-blur-sm cursor-pointer outline-0 border-0 bg-[rgb(51,55,59,0.37)] rounded-[15px] py-2 pl-2.5 md:pr-8 pr-2.5 text-xs mb-3 w-full">
       <div className="flex justify-start items-center sm:w-3/5 w-full">
-        <div className="flex items-center gap-3 w-1/3">
+        <div className="flex items-center space-x-3 w-1/3">
           <img className="w-10 h-10 rounded-lg object-cover" src={img} alt="banner" />
           <img className="md:block hidden" src={heart} alt="heart" />
         </div>
-        <div className="flex md:flex-row flex-col md:justify-between justify-start gap-3 w-2/3">
+        <div className="flex md:flex-row flex-col md:justify-between justify-start md:space-x-3 space-x-0 md:space-y-0 space-y-3 w-2/3">
           <p className="truncate">{title} - {artist}</p>
           <p className="">{category}</p>
         </div>
       </div>
-      <div className="flex md:flex-row flex-col-reverse justify-end items-end md:gap-12 gap-2 w-2/5">
+      <div className="flex md:flex-row flex-col-reverse justify-end items-end md:space-x-12 space-x-0 md:space-y-0 space-y-2 w-2/5">
         <p>{millisecondsToMinute(duration)}</p>
         <img src={more} alt="" />
       </div>
